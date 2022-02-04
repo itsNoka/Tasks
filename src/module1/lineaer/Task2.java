03332bae9a0bf05d15a6fcfced5d72e725665f16
noka <noka082@yandex.ru>
2022-02-04 16:17:03 +0300

    Add task2

---
+++ src/module1/lineaer/Task2.java
@@ -0,0 +1,22 @@
+/*
+Найдите значение функции: (b + sqrt((b ^ 2 ) + 4 * a * c)/ 2 * a) - (a ^ 3) * c +( b ^ - 2).
+ */
+
+package module1.lineaer;
+
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите а");
+        double a = scanner.nextDouble() ;
+        System.out.println("введите b");
+        double b = scanner.nextDouble() ;
+        System.out.println("введите c");
+        double c = scanner.nextDouble();
+
+            System.out.println(method2(a,b,c));
+
+    }
+
+}

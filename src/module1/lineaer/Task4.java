7e0b94440d54417d7bba46003c72a37434457ac8
noka <noka082@yandex.ru>
2022-02-07 15:44:09 +0300

    Add task4

---
+++ src/module1/lineaer/Task4.java
@@ -0,0 +1,15 @@
+/*Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях). Поменять местами
+дробную и целую части числа и вывести полученное значение числа.
+*/
+package module1.lineaer;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] agrs) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Введите число");
+
+    }
+
+}

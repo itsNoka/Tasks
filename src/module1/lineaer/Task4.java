e3777c4336ee4336fa6d54e9d45a5f8df3ea2c4f
noka <noka082@yandex.ru>
2022-02-14 21:03:39 +0300

    Add task4

--- src/module1/lineaer/Task4.java
+++ src/module1/lineaer/Task4.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] agrs) {
-        Scanner scanner = new Scanner (System.in); //подтягиваем сканер
+        Scanner scanner = new Scanner(System.in); //подтягиваем сканер
         System.out.println("Введите число в формате ццц,ддд"); // просто вывод текста
         double a = scanner.nextDouble(); // ввод числа с клавиатуры
         System.out.println(method(a));  // передаем число методу?
 
     }
-    public static double method (double a) { // объявлениие метода
-        if (a<1000) {
-        double b = (a * 1000) % 1000 + (int) a / 1000.0;
 
-        return b;}
-        else {
+    public static double method(double a) { // объявлениие метода
+        if (a < 1000) {
+            double b = (a * 1000) % 1000 + (int) a / 1000.0;
+
+            return b;
+        } else {
             System.out.println("Введите число меньше 1000");
         }
         return a;
     }
-
 }

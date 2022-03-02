4cd11c9660737cb948aba28f689aa6568ccf6e80
noka <noka082@yandex.ru>
2022-03-02 20:05:52 +0300

    Add task3

---
+++ src/module1/cycles/Task3.java
@@ -0,0 +1,14 @@
+/*Найти сумму квадратов первых ста чисел.
+ */
+package module1.cycles;
+
+public class Task3 {
+    public static void main(String[] args) {
+        double sum=0;
+        for (int i=0; i<=100; i++){
+            sum+=Math.pow(i,2);
+            System.out.println(sum);
+        }
+        System.out.println("Сумма первых ста чисел ="+sum);
+    }
+}
